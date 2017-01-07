ce6f8d738d05d5913bc660ccdd694355ee4a4c84
Alonshechter <alonhasmail@gmail.com>
2017-01-07 21:03:07 +0200

    Add files via upload

---
+++ PlayerDamage.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using System;
+using UnityStandardAssets.Characters.FirstPerson;
+using UnityEngine.UI;
+using UnityStandardAssets.ImageEffects;
+
+public class PlayerDamage : MonoBehaviour
+{
+    public float m_playerHealth = 100f;
+    public float m_damageToInflictOnHit;
+    public bool m_isDead;
+    private float m_originalPlayerHealth;
+    public Canvas can;
+    public Slider slide;
+   
+        
+
+
+
+    void Start()
+    {
+        m_isDead = false;
+        m_originalPlayerHealth = m_playerHealth;
+       
+    }
+
+
+    void OnCollisionStay(Collision collision)
+    {
+        if (m_isDead)
+        {
+            return;
+        }
+
+        if (collision.gameObject.name.ToLower().Contains("enemy"))
+        {
+            DamagePlayer ( );
+            slide.value = m_playerHealth;
+            
+        }
+    }
+
+
+    void OnCollisionExit()
+    {
+        Camera.main.GetComponent<Fisheye>().enabled = false;
+    }
+
+
+    void Update()
+    {
+        if (m_isDead)
+        {
+            return;
+        }
+
+        if (m_playerHealth < 0)
+        {
+            KillPlayer ( );
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            RevivePlayer ( );
+        }
+
+       
+    }
+
+
+    private void RevivePlayer ( )
+    {
+        m_playerHealth = m_originalPlayerHealth;
+        m_isDead = false;
+        GetComponent<Rigidbody> ( ).isKinematic = false;
+    }
+
+
+    private void KillPlayer ( )
+    {
+        Debug.Log ( name + " is dead." );
+        m_isDead = true;
+        GetComponent<Rigidbody> ( ).isKinematic = true;
+        this.GetComponent<RigidbodyFirstPersonController>().enabled = false;
+        can.gameObject.SetActive(true);
+;        
+    }
+
+
+    private void DamagePlayer ( )
+    {
+        Debug.Log ( name + " is being hit by enemy" );
+        m_playerHealth -= m_damageToInflictOnHit;
+        Camera.main.GetComponent<Fisheye>().enabled = true;
+     
+    }
+}
---
+++ activayeSound.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class activayeSound : MonoBehaviour {
+
+    private AudioSource damage;
+
+	// Use this for initialization
+	void Start () {
+        damage = GetComponent<AudioSource>();
+        Bullet.OnBulletHit += playSound;
+
+
+    }
+	
+	// Update is called once per frame
+	public void playSound(Bullet bullet, Collision collision)
+        {
+
+        this.transform.position = collision.transform.position;
+        if(collision.gameObject.CompareTag("enemy"))
+        {
+            damage.Play();
+        }
+        
+    }
+}
